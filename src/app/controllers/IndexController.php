855e802dc381bf6f6bf77e9c593ab6d4047fab6b
Mohammad Shakeeb <mohammadshakeeb@cedcoss.com>
2022-04-06 10:25:52 +0530

    corr

--- src/app/controllers/IndexController.php
+++ src/app/controllers/IndexController.php
@@ -7,81 +7,102 @@ class IndexController extends Controller
 {
     public function indexAction()
     {
-    //     $url = "http://api.open-notify.org/iss-now.json";
+        // $url = "http://httpbin.org/post";
 
-    //     // Initialize a CURL session.
-    //     $ch = curl_init();
+        // //     // Initialize a CURL session.
+        // $ch = curl_init();
+        // curl_setopt($ch, CURLOPT_URL, $url);
+        // curl_setopt($ch, CURLOPT_POST, 1);
+        // curl_setopt(
+        //     $ch,
+        //     CURLOPT_POSTFIELDS,
+        //     "postvar1=value1&postvar2=value2&postvar3=value3"
+        // );
 
-    //     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-    //     //grab URL and pass it to the variable.
-    //     curl_setopt($ch, CURLOPT_URL, $url);
+        // $server_output = curl_exec($ch);
+        // $res=json_decode($server_output);
+        // echo "<pre>";
+        // print_r( $server_output);
+        // die;
+        //     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-        
-    //     $response = curl_exec($ch);
-        
-    //     $res=json_decode($response);
-    //     echo "<pre>";
-    //    print_r($res);
-    // // echo $res->iss_position->longitude;
+        //     //grab URL and pass it to the variable.
+        //     curl_setopt($ch, CURLOPT_URL, $url);
 
-    //     $url2= "http://api.open-notify.org/astros.json";
-    //     $fz=curl_init();
-    //     curl_setopt($fz, CURLOPT_RETURNTRANSFER, 1);
-    //     curl_setopt($fz, CURLOPT_URL, $url2);
-    //     $result=curl_exec($fz);
-    //     $r=json_decode($result);
-    //     echo "<pre>";
-    //     foreach($r as $k=>$v){
-    //         foreach($v as $f=>$s){
-    //             echo "<pre>";
-    //     print_r($s->name);
-     
 
-    //     }   
-    // }die;
+        //     $response = curl_exec($ch);
+
+        //     $res=json_decode($response);
+        //     echo "<pre>";
+        //    print_r($res);
+        // // echo $res->iss_position->longitude;
+
+        //     $url2= "http://api.open-notify.org/astros.json";
+        //     $fz=curl_init();
+        //     curl_setopt($fz, CURLOPT_RETURNTRANSFER, 1);
+        //     curl_setopt($fz, CURLOPT_URL, $url2);
+        //     $result=curl_exec($fz);
+        //     $r=json_decode($result);
+        //     echo "<pre>";
+        //     foreach($r as $k=>$v){
+        //         foreach($v as $f=>$s){
+        //             echo "<pre>";
+        //     print_r($s->name);
+
+
+        //     }   
+        // }die;
     }
+    /**
+     * function to search a perticular book and display the result
+     */
+    public function searchAction()
+    {
 
-    public function searchAction(){
-
-        $search=$_POST['search'];
+        $search = $_POST['search'];
         // echo $search;
-        $s=explode(" ", $search);
-    //    print_r ($s);
+        $s = explode(" ", $search);
+        //    print_r ($s);
 
-    $final=implode("+", $s);
-    echo $final;
-    echo "<br>";
-     
-    $url="https://openlibrary.org/search.json?q=$final&mode=ebooks&has_fulltext=true";
-    echo $url;
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_URL, $url);
-    $response = curl_exec($ch);
-    $res=json_decode($response);
-    //  echo "<pre>";
-    //    print_r($res);
-    //    die;  
-    // foreach($res->docs as $k =>$v){
-    //     echo "<pre>";
-    //     echo ($v->amazon_id[0]); 
-    //     die;
-    // }
-        $this->view->response=$res;
+        $final = implode("+", $s);
+        echo $final;
+        echo "<br>";
 
-    // die;
-    
+        $url = "https://openlibrary.org/search.json?q=$final&mode=ebooks&has_fulltext=true";
+        echo $url;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        $response = curl_exec($ch);
+        $res = json_decode($response);
+        //  echo "<pre>";
+        //    print_r($res);
+        //    die;  
+        // foreach($res->docs as $k =>$v){
+        //     echo "<pre>";
+        //     echo ($v->amazon_id[0]); 
+        //     die;
+        // }
+        $this->view->response = $res;
+
+        // die;
+
     }
 
-    public function singleAction() {
-        
+    /**
+     * function to display the single detailed page
+     */
+    public function singleAction()
+    {
+
         $key = $this->request->getPost('hidden');
         $imghidden = $this->request->getPost('imghidden');
-        $j=".json";
-    //    print_r($imghidden);
-    //     die; 
-        $url = "https://openlibrary.org".$key.$j;
+        $j = ".json";
+        //    print_r($imghidden);
+        //     die; 
+        $url = "https://openlibrary.org" . $key . $j;
         $ch = curl_init();
         // echo $url;
         // die;
@@ -89,13 +110,12 @@ class IndexController extends Controller
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         $response = curl_exec($ch);
-       
+
         $apidata = json_decode($response);
         echo "<pre>";
         // print_r($apidata);
         // die();
         $this->view->details = $apidata;
         $this->view->url = $imghidden;
-
     }
 }
