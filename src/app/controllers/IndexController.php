1de5edf5cb4913642f6a140f23764cd7c65e942d
Mohammad Shakeeb <mohammadshakeeb@cedcoss.com>
2022-04-06 09:58:28 +0530

    API Practice

---
+++ src/app/controllers/IndexController.php
@@ -0,0 +1,101 @@
+<?php
+
+use Phalcon\Mvc\Controller;
+
+
+class IndexController extends Controller
+{
+    public function indexAction()
+    {
+    //     $url = "http://api.open-notify.org/iss-now.json";
+
+    //     // Initialize a CURL session.
+    //     $ch = curl_init();
+
+    //     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+    //     //grab URL and pass it to the variable.
+    //     curl_setopt($ch, CURLOPT_URL, $url);
+
+        
+    //     $response = curl_exec($ch);
+        
+    //     $res=json_decode($response);
+    //     echo "<pre>";
+    //    print_r($res);
+    // // echo $res->iss_position->longitude;
+
+    //     $url2= "http://api.open-notify.org/astros.json";
+    //     $fz=curl_init();
+    //     curl_setopt($fz, CURLOPT_RETURNTRANSFER, 1);
+    //     curl_setopt($fz, CURLOPT_URL, $url2);
+    //     $result=curl_exec($fz);
+    //     $r=json_decode($result);
+    //     echo "<pre>";
+    //     foreach($r as $k=>$v){
+    //         foreach($v as $f=>$s){
+    //             echo "<pre>";
+    //     print_r($s->name);
+     
+
+    //     }   
+    // }die;
+    }
+
+    public function searchAction(){
+
+        $search=$_POST['search'];
+        // echo $search;
+        $s=explode(" ", $search);
+    //    print_r ($s);
+
+    $final=implode("+", $s);
+    echo $final;
+    echo "<br>";
+     
+    $url="https://openlibrary.org/search.json?q=$final&mode=ebooks&has_fulltext=true";
+    echo $url;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    $response = curl_exec($ch);
+    $res=json_decode($response);
+    //  echo "<pre>";
+    //    print_r($res);
+    //    die;  
+    // foreach($res->docs as $k =>$v){
+    //     echo "<pre>";
+    //     echo ($v->amazon_id[0]); 
+    //     die;
+    // }
+        $this->view->response=$res;
+
+    // die;
+    
+    }
+
+    public function singleAction() {
+        
+        $key = $this->request->getPost('hidden');
+        $imghidden = $this->request->getPost('imghidden');
+        $j=".json";
+    //    print_r($imghidden);
+    //     die; 
+        $url = "https://openlibrary.org".$key.$j;
+        $ch = curl_init();
+        // echo $url;
+        // die;
+        //grab URL and pass it to the variable.
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $response = curl_exec($ch);
+       
+        $apidata = json_decode($response);
+        echo "<pre>";
+        // print_r($apidata);
+        // die();
+        $this->view->details = $apidata;
+        $this->view->url = $imghidden;
+
+    }
+}
